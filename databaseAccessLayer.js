d0c43283693309a49c6f106a348c72f56a5e12d8
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-04 19:06:46 -0800

    first commit

---
+++ databaseAccessLayer.js
@@ -0,0 +1,77 @@
+const database = include('/databaseConnection');
+
+
+async function getAllUsers() {
+	let sqlQuery = `
+		SELECT name, description
+		FROM restaurant;
+	`;
+
+	try {
+		const results = await database.query(sqlQuery);
+		console.log(results[0]);
+		return results[0];
+	}
+	catch (err) {
+		console.log("Error selecting from restaurant table");
+		console.log(err);
+		return null;
+	}
+}
+
+const passwordPepper = "SeCretPeppa4MyMe0ws";
+
+async function addUser(postData) {
+let sqlInsertSalt = `
+INSERT INTO web_user (first_name, last_name, email, password_salt)
+VALUES (:first_name, :last_name, :email, sha2(UUID(),512));
+`;
+let params = {
+first_name: postData.first_name,
+last_name: postData.last_name,
+email: postData.email
+};
+console.log(sqlInsertSalt);
+try {
+const results = await database.query(sqlInsertSalt, params);
+let insertedID = results.insertId;
+let updatePasswordHash = `
+UPDATE web_user
+SET password_hash = sha2(concat(:password,:pepper,password_salt),512)
+WHERE web_user_id = :userId;
+`;
+let params2 = {
+password: postData.password,
+pepper: passwordPepper,
+userId: insertedID
+}
+console.log(updatePasswordHash);
+const results2 = await database.query(updatePasswordHash, params2);
+return true;
+}
+catch (err) {
+console.log(err);
+return false;
+}
+}
+
+async function deleteUser(webUserId) {
+	let sqlDeleteUser = `
+	DELETE FROM web_user
+	WHERE web_user_id = :userID
+	`;
+	let params = {
+		userID: webUserId
+	};
+	console.log(sqlDeleteUser);
+	try {
+		await database.query(sqlDeleteUser, params);
+		return true;
+	}
+	catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+module.exports = { getAllUsers, addUser, deleteUser }
---
+++ databaseConnection.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2/promise');
+
+const is_render = process.env.IS_RENDER || false;
+
+const dbConfigRender = {
+	host: "sql.freedb.tech",
+	user: "freedb_cwan_main",
+	password: "n!?hTMRsJ2GNPM*",
+	database: "freedb_comp2350-week2-A00970304",
+	multipleStatements: false,
+	namedPlaceholders: true
+};
+
+const dbConfigLocal = {
+	host: "sql.freedb.tech",
+	user: "freedb_cwan_main",
+	password: "n!?hTMRsJ2GNPM*",
+	database: "Free DB",
+	multipleStatements: false,
+	namedPlaceholders: true
+};
+
+if (is_render) {
+	var database = mysql.createPool(dbConfigRender);
+}
+else {
+	var database = mysql.createPool(dbConfigLocal);
+}
+
+module.exports = database;
+		
